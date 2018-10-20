073bf4d92b2d87139c59464eab2a6f70ef015b1f
Erquilenne <maks-7777@mail.ru>
2018-10-20 23:08:12 +0300

    corrections for step 8

--- src/games/Progression.php
+++ src/games/Progression.php
@@ -10,11 +10,11 @@ function run_progression()
 {
     $getQuestionAndCorrectAnswer = function () {
         $step = mt_rand(2, 30);
-        $startStep = $step;
-        $hiddenElementPosition = mt_rand(0, 9);
+        $first = mt_rand(2, 50);
+        $hiddenElementPosition = mt_rand(0, LENGTH_OF_PROGRESSION - 1);
         $progressionArray = [];
         for ($i = 0; $i < LENGTH_OF_PROGRESSION; $i++) {
-            $progressionArray[] = $startStep + $step * $i;
+            $progressionArray[] = $first + $step * $i;
         }
         $correctAnswer = $progressionArray[$hiddenElementPosition];
         $progressionArray[$hiddenElementPosition] = '..';
